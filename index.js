952f74d9d92a8b118381d480475f5d2e0278b547
Brad Morgan <jbradmorg16@gmail.com>
2021-12-21 21:32:30 -0500

    working code

--- index.js
+++ index.js
@@ -5,79 +5,13 @@ const fs = require('fs')
 const writeStream = fs.createWriteStream('beer.csv')
 
 // write headers
-//writeStream.write('Beer Name, Style, ABV, Link \n')
+writeStream.write('Location, Beer Name, Style, ABV, Link')
 
 // Woodstock Location
 request("https://woodstock.reformationbrewery.com/", (error, response, html) => {
-    console.log('---WOODSTOCK---')
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html)
-
-        $('.col--flex').each((i, brad) => {
-            const name = $(brad)
-                .find('.beer-card__name, h3')
-                .text()
-                .replace(/\s\s+/g, '')
-            const style = $(brad)
-                .find('.beer-card__style')
-                .text()
-                .replace(/\s\s+/g,'')
-            const ABV = $(brad)
-                .find('.beer-card__abv')
-                .text()
-                .replace(/\s\s+/g,'')
-            const link = $(brad)
-                .find('.beer-card')
-                .attr('href')
-        console.log(name,'\n', style, '\n', ABV, '\n', link, '\n')
-        
-            
-        //writeStream.write(`${name}, ${style}, ${ABV}, ${link} \n`)
-
-        })
-        console.log('------------------')
-
-    }
-})
-
-// Canton location
-request("https://canton.reformationbrewery.com/", (error, response, html) => {
-    console.log('---CANTON---')
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html)
-
-        $('.col--flex').each((i, brad) => {
-            const name = $(brad)
-                .find('.beer-card__name, h3')
-                .text()
-                .replace(/\s\s+/g, '')
-            const style = $(brad)
-                .find('.beer-card__style')
-                .text()
-                .replace(/\s\s+/g,'')
-            const ABV = $(brad)
-                .find('.beer-card__abv')
-                .text()
-                .replace(/\s\s+/g,'')
-            const link = $(brad)
-                .find('.beer-card')
-                .attr('href')
-        console.log(name,'\n', style, '\n', ABV, '\n', link, '\n')
-        
-            
-        //writeStream.write(`${name}, ${style}, ${ABV}, ${link} \n`)
-
-        })
-        console.log('------------------')
-
-        //console.log('Scraping done...')
-    }
-})
-
-request("https://smyrna.reformationbrewery.com/", (error, response, html) => {
-    console.log('---SMYRNA---')
-    if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html)
+        const location = "WOODSTOCK"
 
         $('.col--flex').each((i, brad) => {
             const name = $(brad)
@@ -98,11 +32,79 @@ request("https://smyrna.reformationbrewery.com/", (error, response, html) => {
         //console.log(name,'\n', style, '\n', ABV, '\n', link, '\n')
         
             
-           //writeStream.write(`${name}, ${style}, ${ABV}, ${link} \n`)
+        writeStream.write(`${location}, ${name}, ${style}, ${ABV}, ${link} \n`)
 
         })
+        //console.log('------------------')
+
+    }
+})
+
+// Canton location
+request("https://canton.reformationbrewery.com/", (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html)
+        const location = "CANTON"
+
+
+        $('.col--flex').each((i, brad) => {
+            const name = $(brad)
+                .find('.beer-card__name, h3')
+                .text()
+                .replace(/\s\s+/g, '')
+            const style = $(brad)
+                .find('.beer-card__style')
+                .text()
+                .replace(/\s\s+/g,'')
+            const ABV = $(brad)
+                .find('.beer-card__abv')
+                .text()
+                .replace(/\s\s+/g,'')
+            const link = $(brad)
+                .find('.beer-card')
+                .attr('href')
+        //console.log(name,'\n', style, '\n', ABV, '\n', link, '\n')
+        
+            
+        writeStream.write(`${location}, ${name}, ${style}, ${ABV}, ${link} \n`)
+
+        })
+        //console.log('------------------')
+
         //console.log('Scraping done...')
-        console.log('------------------')
+    }
+})
+
+request("https://smyrna.reformationbrewery.com/", (error, response, html) => {
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html)
+        const location = "SMYRNA"
+
+
+        $('.col--flex').each((i, brad) => {
+            const name = $(brad)
+                .find('.beer-card__name, h3')
+                .text()
+                .replace(/\s\s+/g, '')
+            const style = $(brad)
+                .find('.beer-card__style')
+                .text()
+                .replace(/\s\s+/g,'')
+            const ABV = $(brad)
+                .find('.beer-card__abv')
+                .text()
+                .replace(/\s\s+/g,'')
+            const link = $(brad)
+                .find('.beer-card')
+                .attr('href')
+        //console.log(name,'\n', style, '\n', ABV, '\n', link, '\n')
+        
+            
+        writeStream.write(`${location}, ${name}, ${style}, ${ABV}, ${link} \n`)
+
+        })
+        console.log('Scraping done...')
+        //console.log('------------------')
 
     }
 
